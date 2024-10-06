a4038c2d079275e1f4edc0ab005e69a4ec9b28a4
Ali <baltimuhammad38@gmail.com>
2024-10-06 16:37:31 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+// $(function() {
+//     $("#submit-color").on("click", function(){
+//         const color1 = $("#color1").val();
+//         const color2 = $("#color2").val();
+//        $("body").css("background" , `linear-gradient(90deg, ${color1} 63%, ${color2} 100%)`)
+//        $("h2").text(color1)
+//        $("h3").text(color2)
+//     })
+// });
+
+
+$(function(){
+    $("#Click").on("click",function(){
+        const color1 = $("#color1").val();
+        const color2 = $("#color2").val();
+        $("body").css("background", `linear-gradient(90deg, ${color1} 50%, ${color2} 100%)`)
+        $("h2").text(color1)
+        $("h3").text(color2)
+    })
+});
